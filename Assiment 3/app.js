abfc7e5a846fc5fe7025a6326f8bde7c2a58142f
MuhammadAhmedSiddiqu <ahmedsiddiqui461@gmail.com>
2024-03-05 03:58:33 +0500

    all files

---
+++ Assiment 3/app.js
@@ -0,0 +1,239 @@
+// CHAPTER #9-11
+// QUESTION #01
+
+
+// var city = prompt("enter your city name")
+// if(city =="lahore"){
+//     alert("welcome to ghaddo ka shaher")
+// }
+// else{
+//     document.write("your not elligible")
+// }
+
+// QUESTION #02
+
+// var gender = prompt("enter your gender")
+// if (gender==="male"){
+//     alert("welcome sir")
+// }
+// else if (gender==="female"){
+//     alert("welcome madam")
+// }
+// else {alert("sorry gender not found")}
+
+// QUESTION #03
+// var sgcolor=prompt("enter signal color");
+// if(sgcolor==="red"){console.log("Must stop")}
+// else if(sgcolor==="yellow"){console.log("Ready to move")}
+// else if(sgcolor==="green"){console.log("Move now")}
+
+
+// QUESTION #04
+//  var fuel= prompt("Enter you fuel in litres");
+
+//  if(fuel <  0.25) {
+//     console.log("please refill the fuel in your car");
+    
+//  }
+//  else if (fuel>=0.25){console.log("your fuel is enough")}
+
+// QUESTION #05
+
+// a. var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// b. var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// d. var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// e. if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+
+
+// f. if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// QUESTION #6
+// var english=("English");
+// var urdu=("Urdu");
+// var math=("Math");
+
+
+// var totaleng=(100);
+// var totalurd=(100);
+// var totalmath=(100);
+
+// var obteng=(64);
+// var obturd=(18);
+// var obtmath=(98);
+// var per=((obteng+obtmath+obturd)*(100)/ (totaleng+totalmath+totalurd));
+
+// document.write("<table >");
+
+// document.write("<b>"+"Subject total marks Obtained marks Percentage"+  "</b>");
+// document.write("<tr>");
+// document.write("<td>"+"<b>"+"Subjects"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Total Marks"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Obtained Marks"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Percentage"+"</b>"+ "</td>");
+// document.write("</tr>");
+
+
+// document.write("</table");
+
+// document.write("<table>");
+// document.write("<tr>");
+// document.write("<td>" +english  +"</td>");
+// document.write("<td>" +totaleng  +"</td>");
+// document.write("<td>" +obteng  +"</td>");
+// document.write("<td>" +obteng*100/totaleng+"%"  +"</td>");
+// document.write("</tr>");
+
+
+// document.write("<tr>");
+// document.write("<td>" +urdu  +"</td>");
+// document.write("<td>" +totalurd  +"</td>");
+// document.write("<td>" +obturd  +"</td>");
+// document.write("<td>" +obturd*100/totalurd+"%"  +"</td>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>" +math  +"</td>");
+// document.write("<td>" +totalmath  +"</td>");
+// document.write("<td>" +obtmath  +"</td>");
+// document.write("<td>" +obtmath*100/totalmath+"%"  +"</td>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>"+"</td>");
+// document.write("<td>"+(totaleng+totalmath+totalurd) +"</td>");
+// document.write("<td>"+(obteng+obturd+obtmath) +"</td>");
+// document.write("<td>"+per+ "%"+"</td>");
+
+
+// document.write("<tr>");
+// document.write("<td>"+"</td>");
+
+// document.write("<tr>");
+// document.write("<td>"+"</td>");
+
+// document.write("<tr>");
+// document.write("<td>"+ "MARK SHEET" +"</td>");
+
+
+// document.write("<tr>");
+// document.write("<td>"+ "TOTAL MARKS" +"</td>");
+// document.write("<td>"+(totaleng+totalmath+totalurd) +"</td>");
+
+// document.write("<tr>");
+// document.write("<td>"+ " MARKS OBTAINED" +"</td>");
+// document.write("<td>"+(obteng+obturd+obtmath) +"</td>");
+
+// document.write("<tr>");
+// document.write("<td>"+ " PERCENTAGE" +"</td>");
+// document.write("<td>"+(per)+ "%"+"</td>");
+
+
+
+// document.write("<tr>");
+// document.write("<td>"+ "Grade" +"</td>");
+
+// if (per >=80)
+// {document.write("<td>"+"A-one"+"</td>")}
+
+// if (per >=70)
+// {document.write("<td>"+"A"+"</td>")}
+
+
+// if (per >=60)
+// {document.write("<td>"+"B"+"</td>")}
+
+
+// if (per <50)
+// {document.write("<td>"+"Fail"+"</td>")}
+
+// document.write("<tr>");
+// {document.write("<td>"+"Remarks"+"</td>")}
+
+// if (per>=80){document.write("<td>"+"Exellent"+"</td>")}
+// if (per>=70){document.write("<td>"+"Good"+"</td>")}
+// if (per>=60){document.write("<td>"+"you need yo improve"+"</td>")}
+// if (per<50){document.write("<td>"+"sorry "+"</td>")}
+
+// QUESTION #07
+// var num=prompt("Enter correct number");
+// if (num==="+1"){document.write("“Bingo! Correct answer”.")}
+// else {document.write("“You are close enoughto the Correct answer”.")}
+
+// //QUESTION #8
+// var num = prompt("Enter a number:");
+// if (num % 3 === 0) {
+//   document.write(num + " is divisible by 3");
+// }  
+
+// else{
+//  document.write(num + " is not divisible by 3")
+// }
+
+// // //QUESTION #9
+// var num = prompt("Enter a number:");
+// if (num %2 ===0){
+//     document.write("it is an even number")
+// }
+// else{document.write("it is an odd number")}
+
+// // //QUESTION #10
+// var tem=prompt("enter temperature");
+// if (tem>40){document.write("it is too hot out side ")}
+// else if (tem>30){document.write("The Weather today is Normal ")}
+// else if (tem>20){document.write("Todays weather is cool ")}
+//  else if (tem>10){document.write("“OMG! Today’s weather is so Cool.”")}
+
+
+// QUESYION #11
+
+// var num1 = +prompt("Enter number 1");
+// var opt = prompt();
+// var num2 = +prompt("Enter number 2");
+// if (opt == "+") {
+//      console.log(num1 + num2);
+//      }
+// else if (opt == "-") {
+//     console.log(num1 - num2);
+// }
+// else if (opt == "*") {
+//     console.log(num1 * num2);
+// }
+// else if (opt == "/") { 
+//     console.log(num1/num2); 
+// }
+// else {
+//     console.log("insert a Number please");
+// }
